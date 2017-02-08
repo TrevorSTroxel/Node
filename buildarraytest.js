55b47e0360892b746b8c3e5c1f534e390b4cb8ad
TrevorSTroxel <actroxel@gmail.com>
2017-02-08 21:20:43 +0000

    Add files via upload

---
+++ buildarraytest.js
@@ -0,0 +1,21 @@
+var list = new Array();
+
+//main
+list[0] = 2;
+list[1] = 1;
+list[2] = 4;
+list[3] = 7;
+list[4] = 5;
+list[5] = 8;
+list[6] = 3;
+list[7] = 6;
+list[8] = 10;
+list[9] = 9;
+
+
+var i = 0;
+for (i = 0; i < 10; i++){
+	
+	console.log(list[i]);
+	
+}
